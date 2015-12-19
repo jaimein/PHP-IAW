ccfa3245cf6d0d55e361875f8b127f93d248947d
ubu <ubu@ubu-portatil>
2015-12-19 13:05:20 +0100

    Ejercicios_propuestos-1.2 Hecho y resuelto

---
+++ Ejercicios_propuestos_4.3/1.2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <!--Hacer un programa en PHP que escriba el resultado de la suma de dos variables
+(x = -1 e y = 9, por ejemplo).-->
+        <?php
+        // Definicion de variables
+        $x="-1";
+        $y="9";
+        
+        // La suma de las variables
+        $z=$x+$y;
+        
+        // Mostramos el valor de las variables y el resultado
+        echo "El valor de x es: <strong>$x</strong>";
+        echo "<br>";
+        echo "El valor de y es: <strong>$y</strong>";
+        echo "<br>";
+        echo "El resultado x + y es: <strong>$z</strong>";
+        ?>
+    </body>
+</html>
