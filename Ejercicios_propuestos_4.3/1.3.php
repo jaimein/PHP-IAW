b6cfb1575394723cc360e403780ceac027b42d7c
ubu <ubu@ubu-portatil>
2015-12-19 13:35:34 +0100

    Ejercicios_propuestos-1.3 Hecho y resuelto

---
+++ Ejercicios_propuestos_4.3/1.3.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <!-- Detectar los errores del siguiente programa: -->
+        <?php
+        // 
+        $x = -1;
+        $y = 9 ;//faltaba el punto y coma
+        $suma = $x + $y;//faltaban los $
+        print('El valor de x es <i>'.$x.'</i>');//faltaaba el ; // no estaba bien la variable
+        print ('<br />');//faltaba la orden
+        print("El valor de y es <i>".$y."</i><br/>" );//faltaba el parentesis final // no estaba bien la variable
+        print("La suma es <b><i>".$suma."</i></b><br/>");// no estaba bien la variable
+?> <!--;
+print("La suma es <b><i>$suma</i></b><br />"); Sobra no es su sitio
+        ?> -->
+    </body>
+</html>
