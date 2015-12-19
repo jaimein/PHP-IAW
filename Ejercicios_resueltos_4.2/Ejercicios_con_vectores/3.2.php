c5bec94b2f6b38682025062d34406268c8cdb82e
ubu <ubu@ubu-portatil>
2015-12-19 11:58:17 +0100

    3.2__Vectores Realizado y comprobado funcionamiento

---
+++ Ejercicios_resueltos_4.2/Ejercicios_con_vectores/3.2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // Definicion de las variables
+        $dias[1]="Lunes";
+        $dias[2]="Martes";
+        $dias[3]="Miercoles";
+        $dias[4]="Jueves";
+        $dias[5]="Viernes";
+        $dias[6]="Sabado";
+        $dias[7]="Domingo";
+        
+        // Mostramos el primer dia y el ultimo
+        echo "El primer dia de la semana es: $dias[1]";
+        echo "<br>";
+        echo "El ultimo dia de la semana es: $dias[7]";
+        ?>
+    </body>
+</html>
